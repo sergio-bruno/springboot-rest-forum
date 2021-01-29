6ef7b5dc05b67fec924e190face64013d2489e17
Sergio Bruno <sergio.bruno.ti@gmail.com>
2021-01-29 06:07:38 -0300

    Starting the project

---
+++ src/test/java/com/slbruno/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slbruno.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
