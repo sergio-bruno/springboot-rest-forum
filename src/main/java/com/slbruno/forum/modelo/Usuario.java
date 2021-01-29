e41071faafe8b2f35a194e9cba83c92109b79fde
Sergio Bruno <sergio.bruno.ti@gmail.com>
2021-01-29 08:37:49 -0300

    Criação do banco H2 e entidades e mapeamento

--- src/main/java/com/slbruno/forum/modelo/Usuario.java
+++ src/main/java/com/slbruno/forum/modelo/Usuario.java
@@ -1,7 +1,15 @@
 package com.slbruno.forum.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
