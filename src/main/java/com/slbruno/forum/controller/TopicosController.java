e83db35a21cc1298bc18557fbe333d44ce30565d
sergio bruno <sergio.bruno.ti@gmail.com>
2021-01-29 21:03:33 -0300

    Implementação de PUT e DELETE, finalizando o CRUD.

--- src/main/java/com/slbruno/forum/controller/TopicosController.java
+++ src/main/java/com/slbruno/forum/controller/TopicosController.java
@@ -2,14 +2,18 @@ package com.slbruno.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.slbruno.forum.controller.dto.DetalhesDoTopicoDto;
 import com.slbruno.forum.controller.dto.TopicoDto;
+import com.slbruno.forum.controller.form.AtualizacaoTopicoForm;
 import com.slbruno.forum.controller.form.TopicoForm;
 import com.slbruno.forum.modelo.Topico;
 import com.slbruno.forum.repository.CursoRepository;
@@ -47,6 +52,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -56,8 +62,37 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
-		Topico topico = topicoRepository.getOne(id);
-		return new DetalhesDoTopicoDto(topico);
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
 }
--- src/main/java/com/slbruno/forum/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/com/slbruno/forum/controller/form/AtualizacaoTopicoForm.java
@@ -4,18 +4,15 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.slbruno.forum.modelo.Curso;
 import com.slbruno.forum.modelo.Topico;
-import com.slbruno.forum.repository.CursoRepository;
+import com.slbruno.forum.repository.TopicoRepository;
 
-public class TopicoForm {
+public class AtualizacaoTopicoForm {
 
 	@NotEmpty @Length(min = 5)
 	private String titulo;
 	@NotEmpty @Length(min = 10)
 	private String mensagem;
-	@NotEmpty 
-	private String nomeCurso;
 	
 	public String getTitulo() {
 		return titulo;
@@ -29,15 +26,11 @@ public class TopicoForm {
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
-	public String getNomeCurso() {
-		return nomeCurso;
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		return topico;
 	}
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	public Topico converter(CursoRepository cursoRepository) {
-		Curso curso = cursoRepository.findByNome(nomeCurso);	
-		return new Topico(titulo, mensagem, curso);
-	}
-		
+	
 }
