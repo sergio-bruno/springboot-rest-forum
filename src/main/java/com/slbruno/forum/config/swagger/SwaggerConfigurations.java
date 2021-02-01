300ca33663de50853b60105d23d713b2c3b60ff7
sergio bruno <sergio.bruno.ti@gmail.com>
2021-02-01 19:12:09 -0300

    Enviando token JWT no Swagger

--- src/main/java/com/slbruno/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/com/slbruno/forum/config/swagger/SwaggerConfigurations.java
@@ -1,12 +1,16 @@
 package com.slbruno.forum.config.swagger;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.slbruno.forum.modelo.Usuario;
 
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -20,6 +24,15 @@ public class SwaggerConfigurations {
 				.apis(RequestHandlerSelectors.basePackage("com.slbruno.forum"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
-				.ignoredParameterTypes(Usuario.class);
+				.ignoredParameterTypes(Usuario.class)
+				.globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                    .name("Authorization")
+                                    .description("Header para Token JWT")
+                                    .modelRef(new ModelRef("string"))
+                                    .parameterType("header")
+                                    .required(false)
+                                    .build()));
 	}
 }
