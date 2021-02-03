a7978f3e597a7b505f01aaf538931842310747ce
Sergio Bruno <sergio.bruno.ti@gmail.com>
2021-02-03 14:21:28 -0300

    Implementando profile dev

--- src/main/java/com/slbruno/forum/controller/AutenticacaoController.java
+++ src/main/java/com/slbruno/forum/controller/AutenticacaoController.java
@@ -3,6 +3,7 @@ package com.slbruno.forum.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,7 @@ import com.slbruno.forum.controller.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
+@Profile("prod")
 public class AutenticacaoController {
 
 	@Autowired
