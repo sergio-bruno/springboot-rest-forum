67663c7c975cc964cf13d0c2ecaa8206091c7ceb
Sergio Bruno <sergio.bruno.ti@gmail.com>
2021-02-03 14:45:19 -0300

    Iniciando a implementação de testes

--- src/test/java/com/slbruno/forum/ForumApplicationTests.java
+++ src/test/java/com/slbruno/forum/ForumApplicationTests.java
@@ -1,13 +1,17 @@
 package com.slbruno.forum;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("teste...");
 	}
 
 }
